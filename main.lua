1e40f003e01b796953caea1d993a4ab7b4e16b7b
Arasakas <reveillere.sylvain35@gmail.com>
2022-09-12 10:56:43 +0200

    Premières version

---
+++ main.lua
@@ -0,0 +1,15 @@
+
+function love.load()
+end
+
+function love.update()
+end
+
+function love.draw()
+    --- Première raquette---
+    love.graphics.rectangle("fill",1,1,20,80)
+    
+    --- Dexième raquette---
+    love.graphics.rectangle("fill",1,1,20,80)
+
+end
